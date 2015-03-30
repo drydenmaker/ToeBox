266853fa869939028e10d283dc0217dfe98f35f0
alton.crossley <alton.crossley@simmsfishing.com>
2015-03-30 10:42:03 -0600

    adding comment pagination

--- php/comments.php
+++ php/comments.php
@@ -49,6 +49,26 @@
 				) );
 			?>
 		</ol><!-- .comment-list -->
+		
+		<?php 
+		if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+	?>
+        	<nav class="navigation comment-navigation" role="navigation">
+        		<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'toebox-basic' ); ?></h2>
+        		<div class="nav-links">
+        			<?php
+        				if ( $prev_link = get_previous_comments_link( __( 'Older Comments', 'toebox-basic' ) ) ) :
+        					printf( '<div class="nav-previous">%s</div>', $prev_link );
+        				endif;
+        
+        				if ( $next_link = get_next_comments_link( __( 'Newer Comments', 'toebox-basic' ) ) ) :
+        					printf( '<div class="nav-next">%s</div>', $next_link );
+        				endif;
+        			?>
+        		</div><!-- .nav-links -->
+        	</nav><!-- .comment-navigation -->
+	<?php
+	endif;?>
 
 	<?php endif; // have_comments() ?>
 
@@ -56,7 +76,7 @@
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'toebox' ); ?></p>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'toebox-basic' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(array(
